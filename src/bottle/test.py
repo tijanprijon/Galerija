c4981d97bc7a6e4d168c577412b5e20cee6102ad
unknown <tijan.p1@gmail.com>
2020-12-29 17:26:41 +0100

    frame for simple login, managing simple cookies

--- src/bottle/test.py
+++ src/bottle/test.py
@@ -1,13 +1,46 @@
 import bottle   # TODO pip install not working, currently entire bottle.py file copied to rep
+from bottle import get, post, request
 
-bottle.TEMPLATE_PATH.insert(0,'C:\\Users\\Uporabnik\\Tijan\\New folder\\Galerija\\bottle\\view')    # TODO add to gitignore config file or change to relative path
+bottle.TEMPLATE_PATH.insert(0,'C:\\Users\\Uporabnik\\Tijan\\projekt_rac\\Galerija\\src\\bottle\\view')    # TODO add to gitignore config file or change to relative path
+
 @bottle.get('/')
 def hello():
-    #return bottle.template("test12.tpl")
-    if bottle.request.get_cookie("user"):
-        return "Welcome back! Nice to see you again"
+    if request.get_cookie('account'):
+        # user = request.get_cookie('account')
+        return bottle.template("test12.tpl")
+    bottle.redirect('/login')
+
+@get('/login')
+def login():
+    return '''
+        <form action="/login" method="post">
+            Username: <input name="username" type="text" />
+            Password: <input name="password" type="password" />
+            <input value="Login" type="submit" />
+        </form>
+    '''
+
+def check_login(username, password):
+    if username == "Tijan" and password == "Prijon":
+        return True
+
+    usernames = ["Tijan", "Miha"]   # test
+    passwords = ["Prijon", "Tijan", "Miha", "legenda123"]
+    if username in usernames and password in passwords:
+        return True
     else:
-        bottle.response.set_cookie("user", "yes")
-        return "Hello there! Nice to meet you"
+        return False
+
+
+@post('/login')
+def do_login():
+    username = request.forms.get("username")
+    password = request.forms.get("password")
+    if check_login(username, password):
+        bottle.response.set_cookie("account", username)
+        return "<p>Your login information was correct.</p>"
+    else:
+        return "<p>ane<p>"
+
 
 bottle.run(host='localhost', port=8080, debug=True, reloader = True)
